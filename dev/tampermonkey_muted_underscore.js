7299aa026b653f5108ae8fc70a33b235a043eb1a
Hubol Persson-Gordon <hubol@articulate.com>
2025-04-01 13:11:30 -0500

    More tampering

--- dev/tampermonkey_muted_underscore.js
+++ dev/tampermonkey_muted_underscore.js
@@ -8,36 +8,42 @@
 // ==/UserScript==
 
 (function () {
-  const styleElement = document.createElement('style')
+  const styleElement = document.createElement('style');
   styleElement.textContent = `#asciicanvas [data-mute] {
   color: #202020;
   background-color: #202020;
-}`
-  document.head.append(styleElement)
+}`;
+  document.head.append(styleElement);
 
   function updateTableCellElementDatasetMute(el) {
     if (el.textContent === '_') {
-      el.dataset.mute = true
+      el.dataset.mute = true;
     } else {
-      delete el.dataset.mute
+      delete el.dataset.mute;
     }
   }
 
   new MutationObserver((records) => {
     for (const { target } of records) {
       if (target instanceof HTMLTableCellElement) {
-        updateTableCellElementDatasetMute(target)
+        updateTableCellElementDatasetMute(target);
       }
     }
   }).observe(document.getElementById('asciicanvas'), {
     childList: true,
     subtree: true,
     characterData: true,
-  })
+  });
+
+  function updateAllTableCellElements() {
+    document.querySelectorAll('#asciicanvas td').forEach(updateTableCellElementDatasetMute);
+  }
 
   const modal_windowOld = modal_window;
   modal_window = function (...args) {
     modal_windowOld(...args);
-    document.querySelectorAll('#asciicanvas td').forEach(updateTableCellElementDatasetMute);
+    updateAllTableCellElements();
   }
+
+  setInterval(updateAllTableCellElements, 1000);
 })()
